bca0e089887d05d14a64afa4b967668f704109cf
Unknown <calebinvents@gmail.com>
2018-01-07 10:14:53 -0800

    Added dronekit, dronekit SITL, pi camera support
    Added a few more command line arguments. Defaults should be what will be used on the quadcopter.
    Haven't tested pi camera support.

--- main.py
+++ main.py
@@ -1,43 +1,101 @@
-# https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 import argparse
 import cv2
 from pancv import PanCV
+try:
+    from pivideostream import PiVideoStream
+except:
+    usePiCamera = False
+else:
+    usePiCamera = True
+import dronekit_sitl
+from dronekit import connect, VehicleMode
 
 panCv = PanCV()
-panCv.colorLower = (29, 86, 6)
-panCv.colorUpper = (64, 255, 255)
+panCv.colorLower = (90, 150, 80) # HSV for 8 bit: 0-180, 0-255, 0-255
+panCv.colorUpper = (150, 255, 255)
 # panCv.targetRadius = 
 # panCv.cameraFOV =
+resolution = (640, 480)
+framerate = 30
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-args = vars(ap.parse_args())
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-    camera = cv2.VideoCapture(0)
-# otherwise, grab a reference to the video file
-else:
-    camera = cv2.VideoCapture(args["video"])
+    help="path to (optional) video file to use in place of camera feed")
+ap.add_argument("-s", "--sitl", help="Use the SITL (simulated) vehicle instead of physical drone",
+    action="store_true")
+ap.add_argument("-d", "--display", help="Draw each frame on the display",
+    action="store_true")
+ap.add_argument("-a", "--address", help="Connection string aka target address of vehicle to connect to. Default /dev/ttyAMA0",
+    default="/dev/ttyAMA0")
+args = ap.parse_args()
 
+# setup video input
+# if a video path was not supplied, grab the reference to the webcam or pi camera
+if args.video == None:
+    if usePiCamera:
+        camera = PiVideoStream(resolution=resolution, framerate=framerate)
+        camera.start()
+        print("Using pi camera")
+    else:
+        camera = cv2.VideoCapture(0)
+        print("Using webcam")
+else: # otherwise, grab a reference to the video file
+    camera = cv2.VideoCapture(args.video)
+    print("Using video file")
+
+# set up dronekit
+if args.sitl:
+    sitl = dronekit_sitl.start_default()
+    connectionString = sitl.connection_string()
+    print("SITL running on %s" % connectionString)
+    vehicle = connect(connectionString)
+    vehicle.wait_ready(True, timeout=30) # wait until available vehicle parameters have been collected
+else: # connect to drone
+    vehicle = connect(args.address, wait_ready=True, baud=57600)
+# set up updates for attitude
+def attitudeCallback(self, attrName, value):
+    print(vehicle.attitude)
+vehicle.add_attribute_listener('attitude', attitudeCallback)
+    
+# print vehicle info
+print("Some vehicle attribute values:")
+print(" GPS: %s" % vehicle.gps_0)
+print(" Battery: %s" % vehicle.battery)
+print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+print(" Is Armable?: %s" % vehicle.is_armable)
+print(" System status: %s" % vehicle.system_status.state)
+print(" Mode: %s" % vehicle.mode.name) # settable
+
+# main loop
 while True:
-    # grab the current frame
-    grabbed, frame = camera.read()
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if args.get("video") and not grabbed:
-        break
+    if usePiCamera:
+        # if a new frame has been read from camera
+        if camera.framesElapsed > 0: 
+            frame = camera.read()
+        # else wait for frame maybe?
+    else:
+        # grab the current frame
+        grabbed, frame = camera.read()
+        # if we are viewing a video and we did not grab a frame,
+        # then we have reached the end of the video
+        if args.video and not grabbed:
+            break
+        
     panCv.detect(frame)
-
-    panCv.display()
-    key = cv2.waitKey(1) & 0xFF
- 
+    if args.display:
+        panCv.display()
+        
+    key = cv2.waitKey(1) & 0xFF 
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
         break
 
-# cleanup the camera and close any open windows
+# cleanup
 camera.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() # close any open windows
+# Close vehicle object
+vehicle.close()
+# Shut down simulator
+if args.sitl:
+    sitl.stop()
---
+++ pivideostream.py
@@ -0,0 +1,54 @@
+# https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+import cv2
+import time
+ 
+class PiVideoStream:
+    def __init__(self, resolution=(320, 240), framerate=32):
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+            format="bgr", use_video_port=True)
+ 
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.stopped = False
+        self.framesElapsed = 0 # number of frames read from camera since last read() call
+        #lastFrameTime = time.perf_counter()
+        
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+ 
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+            # grab the frame from the stream and clear the stream in
+            # preparation for the next frame
+            self.frame = f.array
+            self.rawCapture.truncate(0)
+            self.framesElapsed += 1
+            
+            # if the thread indicator variable is set, stop the thread
+            # and resource camera resources
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+    def read(self):
+        self.framesElapsed = 0
+        # return the frame most recently read
+        return self.frame
+ 
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
