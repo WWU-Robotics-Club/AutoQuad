b673873b5e0bfa7bc5261e7e50aa9eba0cd077db
Unknown <calebinvents@gmail.com>
2018-02-19 18:27:16 -0800

    Improved stuff and added more features
    Now using python 2.7
    Improved object detection
    Added more drone control stuff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import argparse
+import configparser
 import cv2
+import time
+import sys
 from pancv import PanCV
 try:
     from pivideostream import PiVideoStream
@@ -7,34 +10,43 @@ except:
     usePiCamera = False
 else:
     usePiCamera = True
-import dronekit_sitl
-from dronekit import connect, VehicleMode
-
-panCv = PanCV()
-panCv.colorLower = (90, 150, 80) # HSV for 8 bit: 0-180, 0-255, 0-255
-panCv.colorUpper = (150, 255, 255)
-# panCv.targetRadius = 
-# panCv.cameraFOV =
-resolution = (640, 480)
-framerate = 30
+import dronekit
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to (optional) video file to use in place of camera feed")
-ap.add_argument("-s", "--sitl", help="Use the SITL (simulated) vehicle instead of physical drone",
-    action="store_true")
-ap.add_argument("-d", "--display", help="Draw each frame on the display",
-    action="store_true")
-ap.add_argument("-a", "--address", help="Connection string aka target address of vehicle to connect to. Default /dev/ttyAMA0",
-    default="/dev/ttyAMA0")
-ap.add_argument("-i", "--imgonly", help="Only process images, don't try to connect to quadcopter.",
-    action="store_true")
+ap.add_argument("-c", "--config", help="Defaults to rasberry pi zero config. Options: windows",
+    default="DEFAULT")
 args = ap.parse_args()
+
+# config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+configSection = args.config
+framerate = config.getint(configSection,'framerate')
+display = config.getboolean(configSection, 'display')
+takeoffAlt = config.getint(configSection,'takeoffAlt')
+targetCoords = dronekit.LocationGlobalRelative(config.getfloat(configSection, 'targetLat'),
+    config.getfloat(configSection, 'targetLon'), takeoffAlt)
+startChNum = config.get(configSection,'startChNum')
+useSITL = config.getboolean(configSection, 'sitl')
+DKconnect = config.getboolean(configSection, 'connect')
+# set up image recognition
+panCv = PanCV()
+# HSV 8 bit: 0-180, 0-255, 0-255
+panCv.colorLower1 = (0, 120, 50) 
+panCv.colorUpper1 = (25, 255, 255)
+panCv.colorLower2 = (160, 120, 50) 
+panCv.colorUpper2 = (180, 255, 255)
+panCv.targetRadius = config.getfloat(configSection, 'targetRadius')
+panCv.cameraFOV = config.getfloat(configSection, 'cameraFoV')
+panCv.horizontalRes = config.getint(configSection,'horizontalRes')
+panCv.verticalRes = config.getint(configSection,'verticalRes')
+panCv.resetHistorySize(config.getint(configSection,'historySize'))
 
 # setup video input
 # if a video path was not supplied, grab the reference to the webcam or pi camera
-if args.video == None:
+if config.get(configSection,'video') == '':
+    useVideoFile = False
     if usePiCamera:
         camera = PiVideoStream(resolution=resolution, framerate=framerate)
         camera.start()
@@ -43,36 +55,88 @@ if args.video == None:
         camera = cv2.VideoCapture(0)
         print("Using webcam")
 else: # otherwise, grab a reference to the video file
-    camera = cv2.VideoCapture(args.video)
+    useVideoFile = True
+    camera = cv2.VideoCapture(config.get(configSection,'video'))
     print("Using video file")
 
 # set up dronekit
-if args.imgonly == False:
-    if args.sitl == True:
+# if it should try connecting
+if DKconnect:
+    # if using simulator
+    if useSITL:
+        import dronekit_sitl
+        # set up simulator
         sitl = dronekit_sitl.start_default()
         connectionString = sitl.connection_string()
         print("SITL running on %s" % connectionString)
-        vehicle = connect(connectionString)
-        vehicle.wait_ready(True, timeout=30) # wait until available vehicle parameters have been collected
-    else: # connect to drone
-        vehicle = connect(args.address, wait_ready=True, baud=57600)
+    else:
+        connectionString = config.get(configSection,'address')
+    try: # connect to drone
+        vehicle = dronekit.connect(connectionString, wait_ready=True, baud=57600)
+    # Bad TCP connection
+    except socket.error:
+        print 'Dronekit exception: No server exists!'
+    # Bad TTY connection
+    except exceptions.OSError as e:
+        print 'Dronekit exception: No serial exists!'
+    except :
+        print 'Dronekit exception while connecting.'
     # set up updates for attitude
-    def attitudeCallback(self, attrName, value):
-        print(vehicle.attitude)
-    vehicle.add_attribute_listener('attitude', attitudeCallback)
+    #def attitudeCallback(self, attrName, value):
+    #    print(vehicle.attitude)
+    #vehicle.add_attribute_listener('attitude', attitudeCallback)
         
     # print vehicle info
-    print("Some vehicle attribute values:")
+    print("Vehicle status:")
     print(" GPS: %s" % vehicle.gps_0)
     print(" Battery: %s" % vehicle.battery)
     print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
     print(" Is Armable?: %s" % vehicle.is_armable)
     print(" System status: %s" % vehicle.system_status.state)
-    print(" Mode: %s" % vehicle.mode.name) # settable
+    print(" Mode: %s" % vehicle.mode.name)
+
+    # set up vehicle
+    if not useSITL and (vehicle.parameters['PLND_ENABLED'] != 1 or vehicle.parameters['PLND_TYPE'] != 1):
+        vehicle.parameters['PLND_ENABLED'] == 1
+        vehicle.parameters['PLND_TYPE'] == 1
+        vehicle.commands.upload()
+        print("Enabled precision landing. Please reboot.")
+        time.sleep(2)
+        sys.exit()
+    print("Waiting for vehicle armable")
+    while not vehicle.is_armable:
+        time.sleep(1)
+    print("Vehicle is armable.")
+    
+    # fly vehicle
+    while vehicle.channels[startChNum] < 1700 and not useSITL:
+        print("Waiting for start signal. Current level: "
+            + str(vehicle.channels[config.get(configSection,'startChNum')]))
+        time.sleep(1.5)
+    print("Received start signal")
+    print("Changing mode to guided")
+    vehicle.mode = dronekit.VehicleMode("GUIDED")
+    print("Arming")
+    vehicle.armed = True
+    vehicle.commands.upload()
+    while not vehicle.armed:
+        time.sleep(1)
+    print("Armed")
+    print("Taking off")
+    vehicle.simple_takeoff(takeoffAlt)
+    print("Waiting to reach takeoff altitude")
+    while vehicle.location.global_relative_frame.alt < takeoffAlt*0.95:
+        time.sleep(1)
+    print("Reached takeoff altitude")
+    print("Using gps to go to target")
+    localNED = vehicle.location.local_frame
+    targetNorth = localNED.north + config.getfloat(configSection, 'targetNorth')
+    targetEast = localNED.east + config.getfloat(configSection, 'targetEast')
+    targetDown = localNED.down
 
 # main loop
-while True:
-    if usePiCamera:
+while not DKconnect or vehicle.channels[startChNum] > 1700 or useSITL:
+    if usePiCamera and not useVideoFile:
         # if a new frame has been read from camera
         if camera.framesElapsed > 0: 
             frame = camera.read()
@@ -82,23 +146,75 @@ while True:
         grabbed, frame = camera.read()
         # if we are viewing a video and we did not grab a frame,
         # then we have reached the end of the video
-        if args.video and not grabbed:
+        if useVideoFile and not grabbed:
             break
         
     panCv.detect(frame)
-    if args.display:
+    # todo, call gotoLocalNED or sendLandMsg as needed
+    if display:
         panCv.display()
         
     key = cv2.waitKey(1) & 0xFF 
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
         break
+    if DKconnect and not vehicle.armed:
+        print("Vehicle disarmed. Exiting.")
+        break
 
 # cleanup
 camera.release()
 cv2.destroyAllWindows() # close any open windows
 # Close vehicle object
-vehicle.close()
-# Shut down simulator
-if args.sitl:
-    sitl.stop()
+if config.getboolean(configSection, 'connect'):
+    vehicle.close()
+    if config.getboolean(configSection, 'sitl'):
+        # Shut down simulator
+        sitl.stop()
+
+# http://python.dronekit.io/examples/guided-set-speed-yaw-demo.html
+def gotoLocalNED(north, east, down):
+    """
+    Send SET_POSITION_TARGET_LOCAL_NED command to request the vehicle fly to a specified
+    location in the North, East, Down frame.
+    """
+    msg = vehicle.message_factory.set_position_target_local_ned_encode(
+        0,       # time_boot_ms (not used)
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+        0b0000111111111000, # type_mask (only positions enabled)
+        north, east, down,
+        0, 0, 0, # x, y, z velocity in m/s  (not used)
+        0, 0, 0, # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
+        0, 0)    # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
+    vehicle.commands.upload()
+
+# see http://mavlink.org/messages/common#LANDING_TARGET
+# https://github.com/mavlink/mavlink/blob/master/message_definitions/v1.0/common.xml#L4078
+def sendLandMsg(point):
+    msg = vehicle.message_factory.landing_target_encode(
+        0, # time_boot_ms (not used)
+        0, # target num
+        0, # frame. I don't think this matters for angle_x and y
+        point.angleX, # target radians from center of image
+        point.angleY, # target radians from center of image
+        point.distance, # altitude.  Not supported?
+        point.radians, # size of target in radians
+        point.radians) # size of target in radians
+    vehicle.send_mavlink(msg)
+    vehicle.commands.upload()
+def printMavlink(msg):
+    # https://github.com/dronekit/dronekit-python/issues/270
+    # Send arbitrary data over the Pixhawk's 3DR telemetry radio from DroneKit (in 251 byte chunks)
+    # set target_system and target_component to your GCS's IDs or to 0 for broadcast
+    # GCS ids (maybe) see https://github.com/mavlink/qgroundcontrol/issues/2907
+    systemId = 255 # 0
+    compId = 190 # 0
+    msg = dronekitobj.vehicle.message_factory.file_transfer_protocol_encode(255,190,0,data_to_send)
+    msg.pack(dronekitobj.vehicle.message_factory)
+
+    # Can't use DroneKit's send_mavlink() since it alters the message's "target_system" field  
+    # Instead, access the name-mangled superprivate class variable and write the msg to the autopilot
+    dronekitobj.vehicle._MPVehicle__module.master.mav.send(msg)
--- pancv.py
+++ pancv.py
@@ -3,16 +3,19 @@ from collections import deque
 import numpy as np
 import imutils
 import cv2
+import math
 
 class Point:
-    imgPoint = None
-    imgCentroid = None
-    radius = None
+    imgPoint = None # (x,y) position of moment of detected shape
+    imgCentroid = None # (x,y) position of min enclosing circle
+    radius = None # radius of target in meters
+    radians = None # width of target in radians
+    distance = None # triangulated distance from camera
+    children = None # number of contours found within outer contour
     #time = None
-    #x = None
-    #y = None
-    #z = None
-    #confidence = None
+    angleX = None # radians from center of image
+    angleY = None
+    confidence = None # 0 to 1
 class PanCV:
     camera = None
 
@@ -21,62 +24,108 @@ class PanCV:
     # list of tracked points
     colorLower = None
     colorUpper = None
-    targetRadius = None # For calculating distance
-    frameWidth = 600 # width in pixels to resize frame to
-    cameraFOV = None # Degrees? Radians?
-    historySize = 50
+    targetRadius = None # meters. For calculating distance
+    horizontalRes = None # width in pixels to resize frame to
+    verticalRes = None # height of frame after resizing
+    cameraFOV = None # radians
+    historySize = None
     points = deque(maxlen=historySize)
     lastFrame = None
     lastMask = None
     def resetHistorySize(self, size):
         self.historySize = size
-        points = deque(maxlen=historySize)
+        self.points = deque(maxlen = size)
     def detect(self, frame):
         currentPoint = Point()
      
         # resize the frame, blur it, and convert it to the HSV
         # color space
-        frame = imutils.resize(frame, width=self.frameWidth)
+        frame = imutils.resize(frame, width=self.horizontalRes)
         # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
-        # construct a mask for the color "green", then perform
+        # construct a mask for the color, then perform
         # a series of dilations and erosions to remove any small
         # blobs left in the mask
-        mask = cv2.inRange(hsv, self.colorLower, self.colorUpper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+        mask1 = cv2.inRange(hsv, self.colorLower1, self.colorUpper1)
+        mask2 = cv2.inRange(hsv, self.colorLower2, self.colorUpper2)
+        mask = mask1 + mask2
         
-        self.lastFrame = frame
-        self.lastMask = mask
+        mask = cv2.erode(mask, None, iterations=1)
+        mask = cv2.dilate(mask, None, iterations=1)
+
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)[-2]
-     
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            currentPoint.imgPoint = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            currentPoint.imgCentroid = (int(x),int(y))
-            currentPoint.radius = radius
+        (_, contours, hierarchy) = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(frame, contours, -1, (255, 0, 0), 3)
+        self.checkContours(contours, hierarchy, currentPoint)
+        if currentPoint.confidence > 0:
+            self.setCoords(currentPoint)
+            cv2.putText(frame, "Dist: " + str(currentPoint.distance)[:5], (5,15), 1, 1.0, (0,200,200), 1)
+            cv2.putText(frame, "Children: " + str(currentPoint.children)[:5], (5,30), 1, 1.0, (0,200,200), 1)
+            cv2.putText(frame, "Confidence: " + str(currentPoint.confidence)[:5], (5,45), 1, 1.0, (0,200,200), 1)
+        # update the points queue
+        self.points.appendleft(currentPoint)
 
-            #setCoords(currentPoint) # uncomment when setCoords is implemented
-            # update the points queue
-            self.points.appendleft(currentPoint)
+        self.lastFrame = frame
+        self.lastMask = mask
+        return
+    def checkContours(self, contours, hierarchy, point):
+        # hierarchy is array of arrays containing indexes of contours: [[Next, Previous, First_Child, Parent]]
+        # all within an array. So hierarchy[0][0] corresponds to contours[0], h[0][1] to c[1], and so on
+        # loop through outer contours
+        largest = None
+        largestArea = 0
+        numChildren = None
+        # Search for largest contour
+        for i in range(len(hierarchy[0])):
+            if hierarchy[0][i][3] == -1: # Parent is -1 when is the contour is the outermost
+                area = cv2.contourArea(contours[i])
+                if area > largestArea:
+                    largestArea = area
+                    largest = i
+                    # count number of children. Aka number of rings
+                    numChildren = self.countChildren(hierarchy, i)
+                        
+        if largest != None: # if contour was found
+            ((x, y), radius) = cv2.minEnclosingCircle(contours[largest])
+            moments = cv2.moments(contours[largest])
+            if moments["m00"] != 0:
+                point.imgPoint = (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
+                point.imgCentroid = (int(x),int(y))
+                point.radius = radius
+                # 1 when 3 children. 0.5 when no children
+                point.confidence = 1 - 0.5*(1 - numChildren/3)
+            else:
+                point.confidence = 0
+            point.children = numChildren
         else:
-            return False
+            point.confidence = 0
+
         
-        return True
+    # returns longest chain of descendants of contour hierarchy
+    # https://docs.opencv.org/trunk/d9/d8b/tutorial_py_contours_hierarchy.html
+    def countChildren(self, hierarchy, index):
+        siblingChildren = 0
+        children = 0
+        # add sibling and siblings children/siblings
+        if hierarchy[0][index][0] != -1:
+            siblingChildren = self.countChildren(hierarchy, hierarchy[0][index][0])
+        # add child and child's children/siblings
+        if hierarchy[0][index][2] != -1:
+            children += 1
+            children += self.countChildren(hierarchy, hierarchy[0][index][2])
+        return children if children > siblingChildren else siblingChildren
     def setCoords(self, point): # calculate relative coordinates given a "point"
         # See Point class. Given self.cameraFOV, self.frameWidth,
         # x pixels (point.imgPoint[0]), y pixels (point.imgPoint[1]) and radius (point.radius),
         # populate point's x, y, and z with distance from target
+        radPerPix = np.deg2rad(self.cameraFOV) / self.horizontalRes
+        halfTargetAngle = point.radius * radPerPix
+        point.radians = 2 * halfTargetAngle
+        point.distance = self.targetRadius / math.tan(halfTargetAngle)
+        point.angleX = (point.imgCentroid[0] - self.horizontalRes/2) * radPerPix
+        point.angleX = (point.imgCentroid[1] - self.verticalRes/2) * radPerPix
         return
     def display(self):
         # loop over the set of tracked points
@@ -89,9 +138,10 @@ class PanCV:
             # otherwise, compute the thickness of the line and
             # draw the connecting lines
             thickness = int(np.sqrt(self.historySize / float(i + 1)) * 2.5)
-            cv2.line(self.lastFrame, self.points[i - 1].imgPoint, self.points[i].imgPoint, (0, 0, 255), thickness)
+            cv2.line(self.lastFrame, self.points[i - 1].imgPoint, self.points[i].imgPoint,
+                (0, 0, 255), thickness)
         # only draw circle if the radius meets a minimum size
-        if len(self.points) > 0 and self.points[0].radius > 10:
+        if len(self.points) > 0 and self.points[0].confidence > 0 and self.points[0].radius > 10:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(self.lastFrame, self.points[0].imgPoint, int(self.points[0].radius),
@@ -99,3 +149,4 @@ class PanCV:
             cv2.circle(self.lastFrame, self.points[0].imgCentroid, 5, (0, 0, 255), -1)
         # show the frame to our screen
         cv2.imshow("Frame", self.lastFrame)
+        cv2.imshow("Mask", self.lastMask)
